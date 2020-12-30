0b72a2bbb8c1985ecb7804a348e592bdd5331484
Dan Boswell <dan@amygda.la>
2020-12-30 15:43:41 -0500

    Recursively reveal all connected empty cells, win condition.

--- minesweeper.py
+++ minesweeper.py
@@ -1,3 +1,4 @@
+import itertools
 import string
 
 class Board:
@@ -7,15 +8,36 @@ class Board:
         self._board = list()
         self._revealed = list()
         for i in range(size):
-            self._board.append([" "] * size)
+            self._board.append([' '] * size)
             self._revealed.append([False] * size)
 
     def value(self, x, y):
         return self._board[x][y] if self._revealed[x][y] else '#'
 
     def reveal(self, x, y):
+        """Reveal this cell and, if empty, all connected empty cells and their bordering numbered cells."""
+
+        # Base case for recursion.
+        if self._revealed[x][y]:
+            return
+
         self._revealed[x][y] = True
-        return self.value(x, y)
+        
+        # Not the most efficient recursion, but will do for now...
+        if (self._board[x][y] == ' '):
+            for i in range(x-1, x+2):
+                if i < 0 or i >= board.size:
+                    continue
+                for j in range(y-1, y+2):
+                    if j < 0 or j >= board.size:
+                        continue
+                    self.reveal(i, j)
+
+    def is_revealed(self, x, y):
+        return self._revealed[x][y]
+
+    def is_complete(self):
+        return all(itertools.chain.from_iterable(self._board))
 
 
 def draw(board):
@@ -45,14 +67,22 @@ if __name__ == '__main__':
             y = int(inp[1]) - 1
 
             if (x >= board.size) or (y >= board.size):
-                print('The co-ordinates are outside of the board')
+                print('The co-ordinates are outside of the board.')
+                continue
+
+            if (board.is_revealed(x, y)):
+                print('That cell has already been revealed.')
                 continue
 
             board.reveal(x, y)
             draw(board)
 
+            if board.is_complete():
+                print('Congratulations, you swept all mines and won the game!')
+                break
+
         else:
-            print('I didn\'t understand that')
+            print('I didn\'t understand that.')
 
 
 
