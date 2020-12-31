c7b715e68379f725c8051b1e6b2b0ca378f5aa21
Dan Boswell <dan@amygda.la>
2020-12-31 14:41:48 -0500

    More separation-of-concerns refactoring.

--- minesweeper.py
+++ minesweeper.py
@@ -7,10 +7,28 @@ import string
 import sys
 
 
+class CellTypes(enum.Enum):
+
+    Empty = enum.auto()
+    Number = enum.auto()
+    Mine = enum.auto()
+
+class CellContent:
+
+    def __init__(self, type, value=None):
+        self.type = type
+        self.value = value
+
 class Board:
+
+    # Internally the board is a simple 2-D "matrix" (list-of-lists) of characters: 
+    # 'M' for a mine, ' ' for an empty cell, and '1'..'8' for number 
+    # cells. A separate "matrix" of bools determines which cells have
+    # been revealed by the player and which remain hidden.
 
     def __init__(self, size, mines):
         self.size = size
+
         self._board = list()
         self._revealed = list()
         self._marks = set()
@@ -19,11 +37,13 @@ class Board:
             self._board.append([' '] * size)
             self._revealed.append([False] * size)
 
+        # Populate the mines
         cells = list(itertools.product(range(size), repeat=2))
         random.shuffle(cells)
         for x, y in cells[:mines]:
             self._board[x][y] = 'M'
 
+        # Populate the numbers
         for x in range(size):
             for y in range(size):
                 if self._board[x][y] == 'M':
@@ -37,22 +57,29 @@ class Board:
                 if n > 0:
                     self._board[x][y] = f'{n:d}'
 
-
-    def value(self, x, y):
-        return self._board[x][y]
+    def cell(self, x, y):
+        value = self._board[x][y]
+        if value == 'M':
+            return CellContent(CellTypes.Mine)
+        elif value == ' ':
+            return CellContent(CellTypes.Empty)
+        else:
+            return CellContent(CellTypes.Number, value=value)        
 
     def reveal(self, x, y):
         """Reveal this cell and, if empty, all connected empty cells and their bordering numbered cells."""
         if (self._board[x][y] == ' '):
             self._flood_reveal(x, y)
         else:
-            self._revealed[x][y] = True        
+            self._revealed[x][y] = True
+            self._marks.discard((x, y)) 
 
     def _flood_reveal(self, x, y):
         if self._revealed[x][y]:
             return
 
         self._revealed[x][y] = True
+        self._marks.discard((x, y)) 
 
         if self._board[x][y] == ' ':
             for i in range(x-1, x+2):
@@ -71,16 +98,16 @@ class Board:
                    for x in range(self.size)
                    for y in range(self.size))
 
+    def is_marked(self, x, y):
+        cell = (x, y)
+        return cell in self._marks
+
     def toggle_mark(self, x, y):
         cell = (x, y)
         if cell in self._marks:
             self._marks.discard(cell)
         else:
             self._marks.add(cell)
-
-    def is_marked(self, x, y):
-        cell = (x, y)
-        return cell in self._marks
 
 
 class Commands(enum.Enum):
@@ -90,29 +117,27 @@ class Commands(enum.Enum):
     Reveal  = enum.auto()
     Mark    = enum.auto()
 
-class UnknownCommand:
+class Command:
 
-    action = Commands.Unknown
+    def __init__(self, type):
+        self.type = type
 
-class QuitCommand:
+class Turn:
 
-    action = Commands.Quit
-
-class RevealCommand:
-
-    action = Commands.Reveal
-
-    def __init__(self, x, y):
+    def __init__(self, type, x, y):
+        self.type = type
         self.x = x
         self.y = y
 
-class MarkCommand:
 
-    action = Commands.Mark
+class GameConditions(enum.Enum):
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    Lost = enum.auto()
+    Won  = enum.auto()
+
+class InputErrors(enum.Enum):
+    OutOfBounds = enum.auto()
+    Unknown     = enum.auto()
 
 
 class UI:
@@ -124,26 +149,29 @@ class UI:
         for y in range(board.size):
             print(f'{y+1: <2}', sep='', end='')
             for x in range(board.size):
-                if not board.is_revealed(x, y):
+                if board.is_revealed(x, y):
+                    cell = board.cell(x, y)
+                    if cell.type == CellTypes.Mine:
+                        print(' *', sep='', end='')
+                    elif cell.type == CellTypes.Empty:
+                        print('  ', sep='', end='')
+                    else:
+                        print(f' {cell.value:s}', sep='', end='')
+                else:
                     if board.is_marked(x, y):
                         print(' !', sep='', end='')
                     else:
                         print(' #', sep='', end='')
-                else:
-                    value = board.value(x, y)
-                    if value == 'M':
-                        value = "*"
-                    print(f' {value:s}', sep='', end='')
             print()
 
     def input(self):
         try:
             s = input(self._prompt)
         except EOFError:
-            return QuitCommand()
+            return Command(Commands.Quit)
         
         if s.lower() in ('q', 'quit', 'exit'):
-            return QuitCommand()
+            return Command(Commands.Quit)
 
         match = re.match(r'^(!?)([a-zA-Z])([0-9]+$)', s)
         if match:
@@ -152,15 +180,24 @@ class UI:
             y = int(match.group(3)) - 1
 
             if mark:
-                return MarkCommand(x, y)
+                return Turn(Commands.Mark, x, y)
             else:
-                return RevealCommand(x, y)
+                return Turn(Commands.Reveal, x, y)
 
         else:
-            return UnknownCommand
+            return Command(Commands.Unknown)
 
-    def message(self, msg):
-        print(msg)
+    def update(self, condition):
+        if condition == GameConditions.Lost:
+            print('Oh no, you hit a mine! You lost.')
+        elif condition == GameConditions.Won:
+            print('Congratulations, you swept all mines and won the game!')
+
+    def reply(self, reply):
+        if reply == InputErrors.Unknown:
+            print('I didn\'t understand that.')
+        elif reply == InputErrors.OutOfBounds:
+            print('The co-ordinates are outside of the board.')
 
 
 class GameLoop:
@@ -169,36 +206,31 @@ class GameLoop:
         while True:
             command = ui.input()
 
-            if command.action in (Commands.Mark, Commands.Reveal):
-                if (command.x >= board.size) or (command.y >= board.size):
-                    ui.message('The co-ordinates are outside of the board.')
-                    continue
+            if isinstance(command, Turn) and (command.x >= board.size or command.x < 0 or command.y >= board.size or command.y < 0):
+                ui.reply(InputErrors.OutOfBounds)
+                continue
 
-            if command.action == Commands.Quit:
+            if command.type == Commands.Quit:
                 break
 
-            elif command.action == Commands.Mark:
+            elif command.type == Commands.Mark:
                 board.toggle_mark(command.x, command.y)
                 ui.draw(board)
 
-            elif command.action == Commands.Reveal:          
-                if (board.is_revealed(command.x, command.y)):
-                    ui.message('That cell has already been revealed.')
-                    continue                
-
+            elif command.type == Commands.Reveal:                      
                 board.reveal(command.x, command.y)
                 ui.draw(board)
 
-                if board.value(command.x, command.y) == 'M':
-                    ui.message('Oh no, you hit a mine! You lost.')                
+                if board.cell(command.x, command.y).type == CellTypes.Mine:
+                    ui.update(GameConditions.Lost)
                     break
 
                 if board.is_complete():
-                    ui.message('Congratulations, you swept all mines and won the game!')
+                    ui.update(GameConditions.Won)
                     break
 
-            elif command.action == Commands.Unknown:
-                ui.message('I didn\'t understand that.')
+            elif command.type == Commands.Unknown:
+                ui.reply(InputErrors.Unknown)
 
 
 if __name__ == '__main__':
